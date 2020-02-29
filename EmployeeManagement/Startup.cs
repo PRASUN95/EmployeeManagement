f92f8926af38bcba46fe9cdf6f723409f4cd0e56
Prasun Singh <prasunks95@gmail.com>
2020-02-29 22:14:48 +0530

    MVC

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -23,6 +23,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,11 +38,12 @@ namespace EmployeeManagement
             {
                 app.UseExceptionHandler("/Error");
             }
+
             app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
-                throw new Exception("Some error processing your request");
-                await context.Response.WriteAsync("MDW : 2");
+                await context.Response.WriteAsync("Hello Dot Net Core");
             });
         }
     }
