1d20b3369db0aad11af4eb4b3a2fc030660f77ec
Prasun Singh <prasunks95@gmail.com>
2020-03-01 22:36:28 +0530

    Dependency injection

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
 {
     public class HomeController : Controller
     {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
         public JsonResult Index()
         {
-            return Json(new {ID = 1 , name = "MMVC" });
+            return Json(_employeeRepository.GetEmployee(1));
         }
     }
 }
---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employees;
+        public MockEmployeeRepository()
+        {
+            _employees = new List<Employee>
+            {
+                new Employee{Id = 1,Name="John",Department = "ECE",Email="John@gmail.com"},
+                new Employee{Id = 2,Name="Subham",Department = "CS",Email="Subham@gmail.com"},
+                new Employee{Id = 3,Name="Avik",Department = "IT",Email="Avik@gmail.com"},
+                new Employee{Id = 4,Name="Prasun",Department = "CS",Email="Prasun@gmail.com"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employees.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
